bbd4f647ecf8158aadaa77818461cae74103d3dc
MuriloSchali <muriloschali629@gmail.com>
2023-12-15 21:57:12 -0300

    Atualização e Melhorias

--- Components/HomePage.js
+++ Components/HomePage.js
@@ -1,47 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import {
   StyleSheet,
   View,
-  StatusBar,
   Text,
   Image,
+  StatusBar,
   TouchableOpacity,
-} from 'react-native'
-import { WebView } from 'react-native-webview'
+  ActivityIndicator
+} from 'react-native';
+import { WebView } from 'react-native-webview';
+import LottieView from 'lottie-react-native';
 
-const URL = 'https://barbearia.systemmain.com.br/',
-  noConnectionImage = require('../assets/image-error.png')
+const URL = 'https://barbearia.systemmain.com.br/';
+const noConnectionImage = require('../assets/image-error.png');
 
 export default function HomePage() {
-  const [error, setError] = useState(false)
+  const [error, setError] = useState(false);
+  const [errorType, setErrorType] = useState(null);
 
   const handleWebViewLoad = () => {
-    console.log('Página Carregada !')
-  }
+    console.log('Página Carregada !');
+  };
 
   const handleWebViewError = (event) => {
-    setError(true)
-    console.error('Erro no carregamento do WebView:', event.nativeEvent)
-  }
+    setError(true);
+    setErrorType(event.nativeEvent.description);
+    console.error('Erro no carregamento do WebView:', event.nativeEvent);
+  };
 
   const handleTryAgain = () => {
-    setError(false)
-  }
+    setError(false);
+  };
 
   return (
+
+    //Caso não haja conexão com a Internet ! 
     <View style={styles.container}>
+      <StatusBar barStyle="000000" />
       {error && (
         <View style={styles.errorContainer}>
           <Image source={noConnectionImage} style={styles.errorImage} />
-          <Text style={styles.errorText}>
-            <Text style={styles.boldText}>
-              Verifique sua conexão com a Internet!
-            </Text>
-          </Text>
+          <Text style={styles.errorText}>Verifique sua conexão com a Internet !</Text>
+         
           <TouchableOpacity
             onPress={handleTryAgain}
-            style={styles.tryAgainButton}
-          >
+            style={styles.tryAgainButton}>
+
             <Text style={styles.tryAgainButtonText}>Tentar Novamente</Text>
           </TouchableOpacity>
         </View>
@@ -50,21 +54,45 @@ export default function HomePage() {
       {!error && (
         <View style={{ width: '100%', height: '100%' }}>
           <WebView
-            source={{ uri: URL }}
-            onLoad={handleWebViewLoad}
-            onError={handleWebViewError}
-          />
+  source={{ uri: URL }}
+  onLoad={handleWebViewLoad}
+  onError={handleWebViewError}
+  onHttpError={handleWebViewError}
+  startInLoadingState={true}
+
+  renderLoading={() => (
+    <View style={styles.loadingContainer}>
+      <StatusBar backgroundColor="#A9A9A9'" barStyle="dark-content" />
+      < ActivityIndicator size="large"  color="white" />
+    </View>
+  )}
+  
+  renderError={() => (
+    <View style={styles.errorContainer}>
+      <Text style={styles.errorText}>Erro ao carregar a página</Text>
+      <TouchableOpacity onPress={handleTryAgain} style={styles.tryAgainButton}>
+        <Text style={styles.tryAgainButtonText}>Tentar Novamente</Text>
+      </TouchableOpacity>
+    </View>
+  )}
+  
+/>
         </View>
       )}
     </View>
-  )
+  );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#1E1E1E',
+  },
+  loadingContainer: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: '#1E1E1E',
   },
   errorContainer: {
